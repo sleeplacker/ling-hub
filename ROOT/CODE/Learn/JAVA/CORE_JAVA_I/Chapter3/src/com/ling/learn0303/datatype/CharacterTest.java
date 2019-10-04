0c49ad72d23151ed2c4d19f13f5447a2a7d552ec
lingpc <1544650023@qq.com>
2019-10-05 01:21:42 +0800

    add comments

--- ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter3/src/com/ling/learn0303/datatype/CharacterTest.java
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter3/src/com/ling/learn0303/datatype/CharacterTest.java
@@ -3,6 +3,8 @@ package com.ling.learn0303.datatype;
 /**
  * Character类型数据
  *
+ *1.java中的char类型长度为两个字节(16位二进制数)，所以解释了为什么char类型是UTF-16字符
+ *
  * Chapter3/com.ling.learn0303.datatype.CharacterTest.java
  *
  * author lingang
@@ -18,5 +20,6 @@ public class CharacterTest {
 		System.out.println(normalValue);
 		System.out.println(utf16Value);
 		// D:\\users 如果把这里的两个反斜杠换成一个就会有语法错误，因为"反斜杠u"后面必须为4位16进制数字 
+		System.out.println(Integer.toBinaryString(Character.MAX_VALUE)); // 1.java中的char类型长度为两个字节(16位二进制数)，所以解释了为什么char类型是UTF-16字符
 	}
 }
