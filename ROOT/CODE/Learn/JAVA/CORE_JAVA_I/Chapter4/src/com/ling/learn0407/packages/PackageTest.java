71ad59c81f02b0a2e79a7ea8be551ccad0794ae8
lingpc <1544650023@qq.com>
2019-10-14 00:40:23 +0800

    add 0407 package and 1 class

---
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter4/src/com/ling/learn0407/packages/PackageTest.java
@@ -0,0 +1,11 @@
+package com.ling.learn0407.packages;
+
+import java.util.*;
+import java.sql.*;
+
+public class PackageTest {
+	public static void main(String[] args) {
+		// Date d = new Date(); // The type Date is
+		// ambiguous，import的两个包中包含相同类名，使用这个类时会有编译错误
+	}
+}
