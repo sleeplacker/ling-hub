db09cfa875c70750434105d306f5271c212f14dd
lingpc <1544650023@qq.com>
2019-10-17 01:28:20 +0800

    add comments

--- ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter5/src/com/ling/learn0501/protectedtestotherpackage/ProtectedOtherPackageTest.java
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter5/src/com/ling/learn0501/protectedtestotherpackage/ProtectedOtherPackageTest.java
@@ -19,6 +19,7 @@ public class ProtectedOtherPackageTest {
 	public static void main(String[] args) {
 		SubClass1 sub1 = new SubClass1();
 		SubClass2 sub2 = new SubClass2();
+		SuperClassInOtherPackage sub3 = sub2;
 		/* 子类对象和父类不在同一包，子类对象只能访问父类的public域，而不能访问父类的包和protected域 */
 		// System.out.println("sub1.dname = " + sub1.dname);//不可见
 		// System.out.println("sub1.pname = " + sub1.pname);//不可见
@@ -27,6 +28,11 @@ public class ProtectedOtherPackageTest {
 		 * 子类对象和父类在同一包，但是子类和引用子类对象的类不在同一包，只能访问父类的protected和public域，而不能访问包访问权限的域
 		 */
 		// System.out.println("sub2.dname = "+sub2.dname);//不可见
+		/*
+		 * sub2和sub3指向同一对象，由于sub2是SubClass2类的变量，而SubClass2类不在本包，所以不能通过sub2访问包域
+		 * 而sub3是SuperClassInOtherPackage对象，在本包中，所以可以通过sub3访问包域
+		 */
+		System.out.println("sub3.dname = " + sub3.dname);
 		System.out.println("sub2.pname = " + sub2.pname);
 		System.out.println("sub2.name = " + sub2.getName());
 	}
