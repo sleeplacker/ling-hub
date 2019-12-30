aacc7bfdd4de219ec976cf8a80940b967d51fb98
ling <1544650023@qq.com>
2019-12-30 19:05:53 +0800

    mod 1 test

--- ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter14/src/com/ling/learn1409/executor/CompletableFutureTest.java
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_I/Chapter14/src/com/ling/learn1409/executor/CompletableFutureTest.java
@@ -1,5 +1,6 @@
 package com.ling.learn1409.executor;
 
+import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -15,14 +16,30 @@ import java.util.concurrent.ExecutionException;
  */
 public class CompletableFutureTest {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> "begin");
-		cf.thenApply(CompletableFutureTest::doStep2);
+		// CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() ->
+		// 1);
+		CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(CompletableFutureTest::doMainStep);
+		CompletableFuture<String> cs = cf.thenApply(CompletableFutureTest::doStep2);
+		// cs.thenApply(CompletableFutureTest::doStep3);
 		cf.thenApply(CompletableFutureTest::doStep1);
-
+		System.out.println("任务分配完成");
 		System.out.println(cf.get());
+		System.out.println("任务执行完成");
 	}
 
-	public static String doStep1(String str) {
+	public static Integer doMainStep() {
+		System.out.println("开始主任务");
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("完成主任务");
+		return 1;
+	}
+
+	public static String doStep1(Integer str) {
+		System.out.println("开始步骤一");
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
@@ -32,7 +49,8 @@ public class CompletableFutureTest {
 		return null;
 	}
 
-	public static String doStep2(String str) {
+	public static String doStep2(Integer str) {
+		System.out.println("开始步骤二");
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -42,12 +60,14 @@ public class CompletableFutureTest {
 		return null;
 	}
 
-	public static void doStep3(String str) {
+	public static BigDecimal doStep3(String str) {
+		System.out.println("开始步骤三");
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.out.println("完成步骤三");
+		return null;
 	}
 }
