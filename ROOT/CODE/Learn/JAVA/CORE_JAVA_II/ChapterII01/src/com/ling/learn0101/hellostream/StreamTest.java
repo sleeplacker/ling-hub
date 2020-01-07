7b550ade24dc1a1a837b994c50d7d6464c73c478
lingpc <1544650023@qq.com>
2020-01-07 21:20:47 +0800

    begin CORE JAVA II add chapter01 add 1 test

---
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII01/src/com/ling/learn0101/hellostream/StreamTest.java
@@ -0,0 +1,23 @@
+package com.ling.learn0101.hellostream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("row the boat down the stream".split(" "));
+
+		// 传统的遍历list的方法
+		for (String s : list) {
+			System.out.println(s);
+		}
+
+		// 使用流的方式遍历list
+		Stream<String> stream = list.stream();
+		stream.forEach(str -> System.out.println("word : " + str));
+
+		Stream<String> pstream = list.parallelStream();// 并行流
+		pstream.forEach(str -> System.out.println("pword : " + str));// 并行流使用方式和普通流一样，只是底层会并行执行
+	}
+}
