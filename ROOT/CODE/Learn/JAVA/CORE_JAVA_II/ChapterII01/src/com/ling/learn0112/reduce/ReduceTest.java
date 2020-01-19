4b2446584703e852fa9d71672c8c09d88961fc27
lingpc <1544650023@qq.com>
2020-01-19 21:11:58 +0800

    mod 1 test add 2 test

--- ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII01/src/com/ling/learn0112/reduce/ReduceTest.java
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII01/src/com/ling/learn0112/reduce/ReduceTest.java
@@ -1,9 +1,6 @@
 package com.ling.learn0112.reduce;
 
-import java.util.IntSummaryStatistics;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
