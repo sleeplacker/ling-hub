b053f675329556fee22c96d1801a8236c9b98189
ling <1544650023@qq.com>
2020-01-30 00:57:23 +0800

    add 3 test

---
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII02/src/com/ling/learn0204/objectioandserialize/ModDefSerialTest.java
@@ -0,0 +1,166 @@
+package com.ling.learn0204.objectioandserialize;
+
+import java.io.Externalizable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 修改默认的序列化机制
+ * ChapterII02/com.ling.learn0204.objectioandserialize.ModDefSerialTest.java
+ *
+ * author lingang
+ *
+ * createTime 2020-01-28 23:12:23
+ *
+ */
+public class ModDefSerialTest {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+
+		/* 1. transient关键字：使用该关键字修饰的域在对象读写时不会被处理 */
+		Student ling = new Student("Ling", "19911115");
+		Student linzy = new Student("Linzy", "20180903");
+
+		// 写对象
+		ObjectOutput oo = new ObjectOutputStream(new FileOutputStream("D:/filetest/object_content2.txt"));
+		oo.writeObject(ling);
+		oo.writeObject(linzy);
+		/*
+		 * ObjectOutput实现了DataOutput接口，
+		 * 所以可以在对象读写默认行为的基础上再使用DataOutput接口的二进制读写方法来写额外信息
+		 */
+		oo.writeChars(linzy.getBirthday());
+		oo.close();
+
+		// 读对象
+		ObjectInput oi = new ObjectInputStream(new FileInputStream("D:/filetest/object_content2.txt"));
+		Student stu1 = (Student) oi.readObject();
+		Student stu2 = (Student) oi.readObject();
+		String stu2_birthday = "" + oi.readChar() + oi.readChar() + oi.readChar() + oi.readChar() + oi.readChar()
+				+ oi.readChar() + oi.readChar() + oi.readChar();
+		stu2.setBirthday(stu2_birthday);
+		/* birthday域被transient关键字修饰，所以对象流读写时不会包含这个字段，所以读出来的值为空 */
+		System.out.println(stu1);// Student [name=Ling, birthday=null]
+		System.out.println(stu2);// 使用二进制读取方式读入了额外的信息
+
+		/*
+		 * 2. Date类的对象读写：这里记录Date类对象读写的原因是，Date类定义了字节readObject和writeObject方法，
+		 * 这两个方法是private的，但是序列化机制可以调用这两个方法*
+		 */
+		System.out.println("\njava.util.Date类的对象读写：");
+		Date date = new Date();
+		// 写对象
+		ObjectOutput oo2 = new ObjectOutputStream(new FileOutputStream("D:/filetest/object_content3.txt"));
+		oo2.writeObject(date);
+		oo2.close();
+
+		// 读对象
+		ObjectInput oi2 = new ObjectInputStream(new FileInputStream("D:/filetest/object_content3.txt"));
+		Date date1 = (Date) oi2.readObject();
+		System.out.println(date1);
+
+		/*
+		 * 3.
+		 * 自定义对象序列化机制：注意，自定义序列化机制的类必须实现Externalizable接口，并由无参构造器，因为对象输入流会调用无参构造器，
+		 * 然后再调用自定义的readExternal方法
+		 */
+		System.out.println("\n自定义序列化机制：");
+		Student2 tan = new Student2("Txq", "19940924");
+		// 写对象
+		ObjectOutput oo3 = new ObjectOutputStream(new FileOutputStream("D:/filetest/object_content4.txt"));
+		oo3.writeObject(tan);
+		oo3.close();
+
+		// 读对象
+		ObjectInput oi3 = new ObjectInputStream(new FileInputStream("D:/filetest/object_content4.txt"));
+		Student2 stu3 = (Student2) oi3.readObject();
+		System.out.println(stu3);
+	}
+}
+
+@SuppressWarnings("serial")
+class Student implements Serializable {
+	private String name;
+	private transient String birthday;// 声明为transient的域，在对象写入和读出时不会包含
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public Student(String name, String birthday) {
+		this.name = name;
+		this.birthday = birthday;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", birthday=" + birthday + "]";
+	}
+
+}
+
+class Student2 implements Serializable, Externalizable {
+	private String name;
+	private transient String birthday;// 声明为transient的域，在对象写入和读出时不会包含
+
+	public Student2() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public Student2(String name, String birthday) {
+		this.name = name;
+		this.birthday = birthday;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", birthday=" + birthday + "]";
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {// 自定义写对象方法
+		out.writeUTF(name);
+		out.writeUTF(birthday);
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {// 自定义读对象方法
+		name = in.readUTF();
+		birthday = in.readUTF();
+	}
+
+}
---
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII02/src/com/ling/learn0204/objectioandserialize/SerialVersionTest.java
@@ -0,0 +1,135 @@
+package com.ling.learn0204.objectioandserialize;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/**
+ * 序列化版本管理
+ *
+ * ChapterII02/com.ling.learn0204.objectioandserialize.SerialVersionTest.java
+ *
+ * author lingang
+ *
+ * createTime 2020-01-30 00:31:27
+ *
+ */
+public class SerialVersionTest {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		OldClass1 linzy = new OldClass1("Linzy", "20180903");
+		/*
+		 * 1. 没使用serialVersionUID的情况：如果将对象写入文件后，类的指纹有改动(即类、超类、接口、域类和方法签名有改动)，
+		 * 那么再从文件读取文件时就会出现java.io.InvalidClassException
+		 */
+		// 写对象--第一遍执行下面4行，第二遍执行时注释下面4行
+		// ObjectOutput oo = new ObjectOutputStream(new
+		// FileOutputStream("D:/filetest/object_version.txt"));
+		// oo.writeObject(linzy);
+		// oo.close();
+
+		// 读对象-第一遍执行时注释下面3行，第二遍执行下面3行，并将OldClass1类的域name你改为name1(引用该域的地方一起改)
+		// ObjectInput oi = new ObjectInputStream(new
+		// FileInputStream("D:/filetest/object_version.txt"));
+		// OldClass1 oc1 = (OldClass1) oi.readObject();
+		// System.out.println(oc1);// java.io.InvalidClassException
+
+		/*
+		 * 2. 有使用serialVersionUID的情况：如果将对象写入文件后，类的指纹有改动(即类、超类、接口、域类和方法签名有改动)，
+		 * 那么再从文件读取文件时就会出现java.io.InvalidClassException
+		 */
+		// OldClass2 linzy2 = new OldClass2("Linzy", "20180903");
+		// // 写对象--第一遍执行下面4行，第二遍执行时注释下面4行
+		// ObjectOutput oo2 = new ObjectOutputStream(new
+		// FileOutputStream("D:/filetest/object_version2.txt"));
+		// oo2.writeObject(linzy2);
+		// oo2.close();
+
+		// 读对象-第一遍执行时注释下面3行，第二遍执行下面3行，并在OldClass2类中新增age域，并相应地修改toString方法
+		ObjectInput oi2 = new ObjectInputStream(new FileInputStream("D:/filetest/object_version2.txt"));
+		OldClass2 oc2 = (OldClass2) oi2.readObject();
+		System.out.println(oc2);// 读取正常，OldClass2 [name=Linzy,
+								// birthday=20180903,
+								// age=0]，age取默认值0，如果去掉birthday字段，那读取对象会少birthday字段
+	}
+}
+
+@SuppressWarnings("serial")
+class OldClass1 implements Serializable {
+	private String name;
+	private String birthday;
+
+	public OldClass1(String name, String birthday) {
+		this.name = name;
+		this.birthday = birthday;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	@Override
+	public String toString() {
+		return "OldClass1 [name=" + name + ", birthday=" + birthday + "]";
+	}
+
+}
+
+class OldClass2 implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 5135272122814187509L;
+	private String name;
+	private String birthday;
+	private int age;
+
+	public OldClass2(String name, String birthday) {
+		this.name = name;
+		this.birthday = birthday;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "OldClass2 [name=" + name + ", birthday=" + birthday + ", age=" + age + "]";
+	}
+
+}
