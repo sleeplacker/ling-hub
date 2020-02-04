3c33baefb958feea726dec461d60711e8ea67295
ling <1544650023@qq.com>
2020-02-05 01:32:09 +0800

    add 3 test

---
+++ ROOT/CODE/Learn/JAVA/CORE_JAVA_II/ChapterII02/src/com/ling/learn0206/rammapfile/MapBufferConstruction.java
@@ -0,0 +1,24 @@
+package com.ling.learn0206.rammapfile;
+
+/**
+ * 缓冲区数据结构
+ * 
+ * 1. 容量
+ * 2. 读写位置
+ * 3. 界限
+ * 4. 标记
+ * 
+ * 且满足条件：0≤标记≤读写位置≤界限≤容量
+ *
+ * ChapterII02/com.ling.learn0206.rammapfile.MapBufferConstruction.java
+ *
+ * author lingang
+ *
+ * createTime 2020-02-05 00:20:01 
+ *
+ */
+public class MapBufferConstruction {
+	public static void main(String[] args) {
+		// 详细参考103页
+	}
+}
