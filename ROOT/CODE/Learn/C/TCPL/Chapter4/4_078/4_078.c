19730d09156c684669ce2ead1014e2c37277bda5
lingang-nuc-manj <1544650023@qq.com>
2021-05-27 10:09:05 +0800

    mod 1 c file

--- ROOT/CODE/Learn/C/TCPL/Chapter4/4_078/4_078.c
+++ ROOT/CODE/Learn/C/TCPL/Chapter4/4_078/4_078.c
@@ -1,13 +1,15 @@
 /* 条件包含，可以在预处理中选择包含不同的头文件 */
 #include <stdio.h>
 
-#define XXX MSDOS
+#define SYSTEM 2
 
-#if SYSTEM==1
+/* 之所以下面的判断是真，是因为SYSTEM和SYSV都是未定义的，所以两者相等 */
+// #if SYSTEM==SYSV
+#if SYSTEM == 1
 #define HDR "sysv.h"
-#elif SYSTEM==BSD
+#elif SYSTEM == 2
 #define HDR "bsd.h"
-#elif SYSTEM==MSDOS
+#elif SYSTEM == 3
 #define HDR "msdos.h"
 #else
 #define HDR "default.h"
